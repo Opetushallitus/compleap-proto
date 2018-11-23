ca41b6e908f789d3687f6615452171f32fdd99a1
Juhamatti Rintaluoma <juhamatti.rintaluoma@gmail.com>
2018-11-23 15:03:52 +0200

    Bootstrap lisätty

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,10 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import './style/typography.scss';
+@import './style/variables.scss';
+@import './style/bootstrap-custom/custom.scss';
+
 #nav {
   padding: 30px;
   a {
