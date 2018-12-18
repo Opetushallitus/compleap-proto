a253240da19bb40da97d1a30de1ec3b871a1ae3e
Juhamatti Rintaluoma <juhamatti.rintaluoma@gmail.com>
2018-12-18 15:30:14 +0200

    Added error page

---
+++ src/views/Error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="content mt-7 d-flex flex-column align-items-center">
+      <span style="font-size: 6em;">🛑</span>
+      <h1 class="pt-7">
+        Nothing here 😓
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error'
+}
+</script>
+
+<style lang="scss">
+@import '../style/bootstrap-custom/custom.scss';
+.nav {
+  display: none;
+}
+</style>
